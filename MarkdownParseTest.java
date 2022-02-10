10fb55f834f4fad44339f35697036bc9fd942541
brighyama <brighyama@gmail.com>
2022-02-09 19:39:44 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -128,6 +128,7 @@ public class MarkdownParseTest {
         assertEquals(this.strList, List.of());
     }
 }
+//change
 
 // javac -cp "lib/\*" MarkdownParseTest.java
 // java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
