ca034d4991a0893f5d0bdf216b1f8bfb7d859c5e
brighyama <brighyama@gmail.com>
2022-02-16 00:21:24 -0800

    update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,12 +1,9 @@
-/** 
+
 // File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Stack;
-import java.util.HashMap;
-
 
 public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
@@ -22,7 +19,7 @@ public class MarkdownParse {
         //Then we look for an open paren but there are no more open paren
         //This results in an infinite loop (which is the error we are gett)
 
-        Stack<String> parenParity = new Stack<>();
+
         //if we find an open bracket/parenthesis push into stack
         //if we find a closing bracket/parenthesis and the top of the stack is 
         /*
@@ -41,7 +38,7 @@ public class MarkdownParse {
         System.out.println(myStr.contains("e"));     // true
         System.out.println(myStr.contains("Hi"));    // false
         */  
-        /** 
+
         String[] imageExtensions = { ".jpg", ".png", ".jpeg", ".raw", ".jfif", ".pjpeg", ".pjp", ".avif", ".gif", ".svg", ".webp" };
 
         int currentIndex = 0;
@@ -78,52 +75,6 @@ public class MarkdownParse {
         return toReturn;
     }
     
-    public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
-    }
-}
-
-*/
-
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-
-public class MarkdownParse {
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            System.out.format("%d\t%d\t%s\n", currentIndex, nextOpenBracket, toReturn);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            System.out.println(currentIndex);
-            if(nextOpenBracket == -1 || nextCloseBracket == -1
-                  || closeParen == -1 || openParen == -1) {
-                return toReturn;
-            }
-            String potentialLink = markdown.substring(openParen + 1, closeParen);
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
-                toReturn.add(potentialLink);
-                currentIndex = closeParen + 1;
-            }
-            else {
-                currentIndex = currentIndex + 1;
-            }
-        }
-        
-
-        return toReturn;
-    }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
